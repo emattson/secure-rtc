6649f32988f284f9de8d4cf8af36de7399273855
Eli <elimattson13@gmail.com>
2015-09-19 01:02:57 -0700

    should express that it has key pair

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 var SimplePeer = require('simple-peer');
 var inherits = require('inherits');
+var kbpgp = require('kbpgp');
 
 function Peer(options) {
+  options = options == undefined ? {} : options;
+  //constructor based on simple-peer
   SimplePeer.call(this, options);
+  this.id = options.id !== undefined ? options.id : (Math.random() * 99999999).toString(16);
+  //keyring
+  this.ring = new kbpgp.keyring.KeyRing();
+  //use ecc
+  this.ecc = options.ecc !== undefined ? options.ecc : false;
 }
 
 inherits(Peer, SimplePeer);
 
-
+Peer.prototype._generateKeys = function (callback) {
+  var params = {
+    //asp: my_asp,
+    userid: this.id
+  };
+  kbpgp.KeyManager.generate_ecc(params, function (err, peer) {
+    if (err != null) {
+      console.log('Problem: ', err);
+    } else {
+      // sign peer's subkeys
+      peer.sign({}, function (err) {
+        if (!err) {
+          //me = peer;
+          this.ring.add_key_manager(peer);
+          this._hasKeys = true;
+        }
+      }.bind(this));
+    }
+  }.bind(this));
+};
 module.exports = Peer;
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,29 @@
 var Peer = require('../src/index');
 var SimplePeer = require('simple-peer');
 
-describe('simple-rtc', function() {
-  it('should inherit properties from simple-peer', function() {
-    var peer = new Peer();
-    var simplePeer = new SimplePeer();
-    expect(peer).to.contain.all.keys(Object.keys(simplePeer))
+describe('secure-rtc', function() {
+  describe('webrtc', function() {
+    it('should inherit properties from simple-peer', function () {
+      var peer = new Peer();
+      expect(peer).to.be.an('object');
+      expect(peer instanceof SimplePeer).to.be.ok;
+    });
+  });
+
+  describe('key generation', function() {
+    it('_generateKeys should express that it has a public/private keypair', function() {
+      var peer = new Peer({id: 'my super id'});
+      peer._generateKeys(function() {
+        expect(peer._hasKeys).to.be.ok;
+        done();
+      }.bind(peer));
+    });
+
+    it('should call _generateKeys if a public key is not provided in options', function() {
+    });
+
+    it('should provide an asp', function() {
+    });
+
   });
 });
